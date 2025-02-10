1ec6ebe6e002f8978f5d845c65a2cdad3cc0e277
martim-lusofona <martim.mourao@ulusofona.pt>
2025-02-10 19:07:18 +0000

    Refactor user creation logic to set unusable password for new users

--- class_attendance/utils.py
+++ class_attendance/utils.py
@@ -1,13 +1,12 @@
 from django.conf import settings
-from django.utils.crypto import get_random_string
 from django.contrib.auth import get_user_model 
-
 
 def getUserOrCreate(email):
     UserModel = get_user_model()
     if not UserModel.objects.filter(email=email).exists():
-        raw_password = get_random_string(30) # the user must reset the password to login (we dont care about this password)
-        user = UserModel.objects.create_user(email=email, password=raw_password)
+        user = UserModel.objects.create(email=email)
+        user.set_unusable_password()
+        user.save()
     else:
         user = UserModel.objects.get(email=email)
 
