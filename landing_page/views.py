3ce38d414ca766afeae6bff7c20b81ffc14f9333
martim-lusofona <martim.mourao@ulusofona.pt>
2025-05-30 23:42:07 +0100

    feat: Add redirect for authenticated users in landing page view

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def landing_page_view(request):
+    if request.user.is_authenticated:
+        return redirect('/class_attendance/universities')
+    
     return render(request, "landing_page/landing_page.html")
